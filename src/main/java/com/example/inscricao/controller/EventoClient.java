108c91f52546953e4e5a7fc8c32b31f67e153c03
arthurestevao <arthursousacc@gmail.com>
2026-06-02 12:33:42 -0300

    configurando

--- src/main/java/com/example/inscricao/controller/EventoClient.java
+++ src/main/java/com/example/inscricao/controller/EventoClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "eventos", url = "${feign.url}")
+@FeignClient(name = "ms-evento")
 public interface EventoClient {
 
     @GetMapping("/{id}")
